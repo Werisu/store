069ff6786c172d55ab39ca74a77dfec5b4b69029
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-01-04 09:16:13 -0300

    feat(products): add lib

---
+++ modules/products/src/lib/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'store-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css',
+})
+export class ProductsComponent {}
