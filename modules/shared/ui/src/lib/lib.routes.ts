adb59275b87d7e790551b5625c8bf7d0b4a774c5
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-01-04 09:19:56 -0300

    feat: add shared-ui lib

---
+++ modules/shared/ui/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { SharedUiComponent } from './shared-ui/shared-ui.component';
+
+export const sharedUiRoutes: Route[] = [
+  { path: '', component: SharedUiComponent },
+];
