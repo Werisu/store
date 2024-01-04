bfcc1eed8aeb9c7c60c504d61da6c0eea5a780fb
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-01-04 09:23:32 -0300

    feat: shared ui integration

--- modules/orders/src/lib/orders/orders.component.ts
+++ modules/orders/src/lib/orders/orders.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedUiComponent } from '@store/shared-ui';
 
 @Component({
   selector: 'store-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedUiComponent],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css',
 })
--- modules/products/src/lib/products/products.component.ts
+++ modules/products/src/lib/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedUiComponent } from '@store/shared-ui';
 
 @Component({
   selector: 'store-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedUiComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
